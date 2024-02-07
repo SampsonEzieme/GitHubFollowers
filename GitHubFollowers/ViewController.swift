e5c2be624fda4d0ed42a6441c83c3e8b85055bf5
Sampson <siezieme@ucdavis.edu>
2024-02-06 18:20:56 -0800

    Initial Project

---
+++ GitHubFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubFollowers
+//
+//  Created by Sampson Ezieme on 2/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
