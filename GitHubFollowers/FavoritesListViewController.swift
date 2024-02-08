6cb097cfd0de8be7077cb6c096fca1f99b2a44e3
Sampson <siezieme@ucdavis.edu>
2024-02-07 22:48:13 -0800

    Set Up Navigation and Tab Bars

---
+++ GitHubFollowers/FavoritesListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FavoritesListViewController.swift
+//  GitHubFollowers
+//
+//  Created by Sampson Ezieme on 2/7/24.
+//
+
+import UIKit
+
+class FavoritesListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+    
+
+}
---
+++ GitHubFollowers/SearchViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SearchViewController.swift
+//  GitHubFollowers
+//
+//  Created by Sampson Ezieme on 2/7/24.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+    
+
+}
