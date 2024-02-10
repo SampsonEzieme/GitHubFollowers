85a590569e0efc6ab93b33c174dbebff52db3b2c
Sampson <siezieme@ucdavis.edu>
2024-02-09 17:09:06 -0800

    create UIButton and UITextField

--- GitHubFollowers/GFButton.swift
+++ GitHubFollowers/GFButton.swift
@@ -11,12 +11,19 @@ class GFButton: UIButton {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
-        //customization
+
+        configure()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
     }
     
     private func configure() {
